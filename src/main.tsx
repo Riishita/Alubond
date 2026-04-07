e80dd52391432983f8d088ee80380345d3cd8a8f
Rishita <112199315+Riishita@users.noreply.github.com>
2026-04-07 08:24:16 +0530

    c

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./styles/gradients.css";
+import "./Styles/gradients.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
